039902c53f130dda14301724368ef922dd6b6a77
pethuel <111269767+Pethuel-joel@users.noreply.github.com>
2024-02-11 09:37:20 +0100

    Add files via upload

---
+++ laptop_tech.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def submit():
+    name = name_entry.get()
+    phone = phone_entry.get()
+    make = make_var.get()
+    model = model_var.get()
